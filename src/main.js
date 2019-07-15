b8416528cc19244975acfe12e191ea850452d477
CrowdYellow <echo61546@gmail.com>
2019-07-15 20:41:15 +0800

    安装axios

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App'
 import router from './router'
 // 引入全部组件
 import Mint from 'mint-ui';
+import axios from 'axios'
 Vue.use(Mint);
 
 Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
 
 /* eslint-disable no-new */
 new Vue({
